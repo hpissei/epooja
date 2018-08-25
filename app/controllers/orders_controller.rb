fab4541d509f313150d082c72b4a4ed75240ac8d
hpissei <hpultimatemedia@gmail.com>
2018-08-25 22:42:56 +0530

    address fixed per order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
 
   def allowed_params
-    params.require(:order).permit(:Book_time,:Book_date,:Pooja_id,:Book_status,:User_id)
+    params.require(:order).permit(:Book_time,:Book_date,:Pooja_id,:Book_status,:User_id,:address)
   end
 
   def create
     @order=Order.create(allowed_params)
     if @order
       flash[:success]="Order placed successfully"
+      #payment gateway call
     else
       puts "error"
       flash[:notice]="Something went wrong!!!"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_24_170405) do
+ActiveRecord::Schema.define(version: 2018_08_25_144625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2018_08_24_170405) do
     t.string "Book_status", limit: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
   create_table "pooja_products", force: :cascade do |t|
