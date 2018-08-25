ece24e5fed728192e647efed6c199bb0fcc06899
hpissei <hpultimatemedia@gmail.com>
2018-08-25 22:58:43 +0530

    seed pooja

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+1.upto(5) do |index|
+  Pooja.new("pooja#{index}","",1000,"","","pooja#{index}")
+end
