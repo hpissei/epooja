8bd557b00eec5e9d5f25ca814ba9134d1f41d4b0
hpissei <hpultimatemedia@gmail.com>
2018-09-02 13:49:29 +0530

    user id and pooja id in order controller updated

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   belongs_to :user
   validates :Book_date, presence: true
   validates :Book_time, presence: true
-  validates :Pooja_id, presence: true
+  validates :pooja_id, presence: true
 
   scope :orders_by_user, -> (user_id){ where(user_id: user_id) }
 end
