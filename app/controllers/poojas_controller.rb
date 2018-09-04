00e423d2663ca48ac6324ac57c61c3e372e61b2c
hpissei <hpultimatemedia@gmail.com>
2018-09-04 21:02:58 +0530

    user required for actions and cart empty page added

--- app/controllers/poojas_controller.rb
+++ app/controllers/poojas_controller.rb
@@ -1,15 +1,22 @@
 class PoojasController < ApplicationController
+  before_action :authenticate_user!, only: ["pooja"]
+
   def index
     @poojas=Pooja.all.paginate(page: params[:page], per_page: 10)
   end
 
   def pooja
-    if params[:id]
-      @pooja=Pooja.find(params[:id])
-      @order=Order.new
-      #@address=CustomerAddress.new
-    else
-      flash[:error]="Pooja not Found!!!"
+    begin
+      if params[:id]
+        @pooja=Pooja.find(params[:id])
+        @order=Order.new
+        #@address=CustomerAddress.new
+      end
+    rescue ActiveRecord::RecordNotFound
+      redirect_to poojas_index_path
+      #render partial: "errors/_404" to redirect to 404 page
+      return
     end
   end
+
 end
